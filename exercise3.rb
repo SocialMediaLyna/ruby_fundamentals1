787aa9f39faf8403adf8074efc0c6a4e84d23530
SweetNAMese <t.lyna.nguyen@gmail.com>
2016-01-12 15:29:15 -0500

    name and age add

--- exercise3.rb
+++ exercise3.rb
@@ -1,4 +1,8 @@
-  puts "What's your name?"
-name = gets.chomp
-name.capitalize!
-  puts "Hi #{name}!"
+puts "What's your name?"
+  name = gets.chomp
+  name.capitalize!
+puts "Hi #{name}!"
+
+puts "How old are you?"
+  age = gets.chomp
+puts "You are #{age}"
