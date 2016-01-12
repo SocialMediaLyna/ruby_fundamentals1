1772a70556021df4c8d8aa335d4f0479b2a3c396
SweetNAMese <t.lyna.nguyen@gmail.com>
2016-01-12 15:56:32 -0500

    If, elsif, else

--- exercise4.rb
+++ exercise4.rb
@@ -2,14 +2,13 @@ for num in 1..100
 
 if num % 3 == 0 && num % 5 == 0
   puts "BitMaker"
-
 elsif num % 3 == 0
     puts "Bit"
 
-  elsif num % 5 == 0
+elsif num % 5 == 0
     puts "Maker"
 
-  else puts num
-
+else puts num
+  
   end
 end
